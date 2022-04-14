b3155b84a344e487c6980e5b0908d311b5656437
Andres Santacruz <andres_v10@live.com>
2022-04-14 02:34:55 -0500

    [FIX]: Change again src to img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ export const Header = () => {
     <header className="border-slate-900 border-b-2 bg-gray-300">
       <nav className="flex p-4 items-center">
         <div className="w-24 ">
-          <img src="../imgs/logo.png" alt="escudo CPCC" className="w-20" />
+          <img
+            src="/assets/logo.344c07fc.png"
+            alt="escudo CPCC"
+            className="w-20"
+          />
         </div>
         <h1 className="text-center my-0 mx-auto sm:text-5xl text-3xl">
           Planeador CPCC
